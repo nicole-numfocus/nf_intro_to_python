121ab972374bc5cdb5547059d21771d71c28f280
Ravin Kumar <ravinsdrive@gmail.com>
2020-06-06 19:34:05 -0700

    Add lesson 1

---
+++ code/1_basics.py
@@ -0,0 +1,20 @@
+# This is a line comment. Python won't run this
+print(2+2)
+
+# Triple quotes is a block comment 
+"""
+print("Py" + "Data")
+
+city = "Austin"
+state = "Texas"
+
+print(city + state)
+"""
+
+# Lists are collections of objects
+nf_staff = ["Leah", "Walker", "Jim"]
+
+# Lets go through for loops
+for staff in nf_staff:
+    print(nf_staff)
+
