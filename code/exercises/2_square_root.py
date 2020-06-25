9fdbb1d9d8675fbd6d724475e0ee566e3a53377b
Ravin Kumar <ravinsdrive@gmail.com>
2020-06-25 16:08:57 -0700

    Add more guidance to square root

--- code/exercises/2_square_root.py
+++ code/exercises/2_square_root.py
@@ -1,3 +1,9 @@
-# Get sqaure roots of numbers
+# Get square roots of each of the numbers below
+
+# Hint: Import the needed python module
 
 numbers = [4, 9, 16, 25]
+
+# The first number can be accessed like this for testing purposes
+first_num = numbers[0]
+print(first_num)
