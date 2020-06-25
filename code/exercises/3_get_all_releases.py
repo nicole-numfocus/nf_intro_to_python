4a1d2c0d0528fcf8a8f6e59b988d259db3132cb6
Ravin Kumar <ravinsdrive@gmail.com>
2020-06-25 16:16:07 -0700

    Update pypi pulls

---
+++ code/exercises/3_get_all_releases.py
@@ -0,0 +1,8 @@
+# Using a python get the last release date and version of multiple python proects
+
+
+# Hint: Go onto pypi and find the xml for pandas, xarray, pymc3, or whatever
+# other libraries you want
+project_xml=[]
+
+# Fill in the code to parse the xml and print the versions
--- code/pypi_pull.py
+++ code/pypi_pull.py
@@ -2,15 +2,19 @@ import xml.etree.cElementTree as ET
 import requests
 
 
-# Project XML
+# XML file as found on pypi website
 project_xml = 'https://pypi.org/rss/project/arviz/releases.xml'
 
-# XML Parsing
+# Steps needed to parse the XML File
+
+# Find the right item in the XML file
 xml_string = requests.get(project_xml).text
 tree = ET.fromstring(xml_string)
 item = tree.find("channel").find("item")
 
+# Get the right item from xml and pull put the publication date and the title
 date = item.find("pubDate").text
-title = item.find("title").text
+version = item.find("title").text
 
-print(project_xml + " " + title + " " + date)
+# Print the XML file, version, and date of release
+print(project_xml + " " + version + " " + date)
